d8b28d0bd882eec476b4afc0ee932ce3f7db43e7
hwq <1102684711@qq.com>
2020-07-21 09:59:52 +0800

    update

--- src/Array-1/index.ts
+++ src/Array-1/index.ts
@@ -1,3 +1,8 @@
+/**
+ * 给定一个整数数组 nums 和一个目标值 target
+ * 请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
+ * 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素不能使用两遍。
+ */
 function twoSum(nums: number[], target: number): number[] {
   const hashMap: { num: number } | any = {};
   const length = nums.length;
@@ -12,4 +17,4 @@ function twoSum(nums: number[], target: number): number[] {
   return result;
 }
 
-console.info(twoSum([2, 7, 22, 45], 24));
+console.info(twoSum([2, 7, 22, 45], 24)); // [0, 2]
--- src/Array-35/index.ts
+++ src/Array-35/index.ts
@@ -1,4 +1,7 @@
-// 二分查找
+/**
+ *给定一个排序数组和一个目标值，在数组中找到目标值，并返回其索引。如果目标值不存在于数组中，返回它将会被按顺序插入的位置。
+ *你可以假设数组中无重复元素。
+ */
 function SearchInsert(nums: number[], target: number): number {
   const len = nums.length;
   let left = 0;
