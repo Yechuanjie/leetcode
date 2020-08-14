65a6a414333a4e5561f84b447b7289352e936868
hwq <1102684711@qq.com>
2020-08-14 11:43:08 +0800

    update

--- README.md
+++ README.md
@@ -1,6 +1,14 @@
-#### algorithm
+#### leetcode 算法题解
 
-> leetcode 算法题解
+项目中每段代码都可以正常运行
+
+```shell
+git clone https://github.com/Yechuanjie/leetcode.git
+
+yarn install
+```
+
+.vscode 中已配置好 launch.json，安装依赖后，任意打开一个 ts 文件，然后在 vscode 中使用 debug 功能，直接启动程序
 
 **注：所有题目均来源于力扣，仅供学习和参考，如需转载，请注明出处**
 
--- src/LinkList/index.ts
+++ src/LinkList/index.ts
@@ -8,10 +8,27 @@
  * 输入：1->2->4, 1->3->4
  * 输出：1->1->2->3->4->4
  */
-import { ListNode } from '../index';
-
-function mergeTwoLists(l1: ListNode | null, l2: ListNode | null): ListNode | null {
-  return null;
+class ListNode {
+  val: number;
+  next: ListNode | null;
+  constructor(val?: number, next?: ListNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.next = next === undefined ? null : next;
+  }
 }
+const mergeTwoLists = (l1: ListNode | null, l2: ListNode | null): ListNode | null => {
+  if (l1 === null) return l2;
+  if (l2 === null) return l1;
+  if (l1.val < l2.val) {
+    l1.next = mergeTwoLists(l1.next, l2);
+    return l1;
+  } else {
+    l2.next = mergeTwoLists(l1, l2.next);
+    return l2;
+  }
+};
 
-console.info(mergeTwoLists([1,2,4], [1,3,4]))
+const list1 = new ListNode(1, new ListNode(2, new ListNode(4)));
+const list2 = new ListNode(1, new ListNode(3, new ListNode(4)));
+
+console.info(mergeTwoLists(list1, list2));
